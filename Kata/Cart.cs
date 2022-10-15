7ff7b6a4eafc8e562e1aadc2f1bfcc473d21d389
Joseph Nutt <JoeNutt01@gmail.com>
2022-10-15 12:01:41 +0100

    sdd in cart logic

--- Kata/Cart.cs
+++ Kata/Cart.cs
@@ -1,21 +1,33 @@
 using Kata.Interfaces;
 
 namespace Kata;
-
+using System.Linq;
 public class Cart : ICart
 {
+    private readonly IPriceCalculator _priceCalculator;
+    private readonly IList<string> _items; 
+
+    public Cart(IPriceCalculator priceCalculator)
+    {
+        _priceCalculator = priceCalculator;
+        _items = new List<string>(); 
+    }
+
     public void AddItem(string item)
     {
-        throw new NotImplementedException();
+       _items.Add(item);
     }
 
     public int toalItems()
     {
-        throw new NotImplementedException();
+        return _items.Count;
     }
 
     public decimal totalCost()
     {
-        throw new NotImplementedException();
+        var groupedItems = _items
+            .GroupBy(x => x)
+            .ToDictionary(item => item.Key, itemCount => itemCount.Count());
+        return groupedItems.Sum(item => _priceCalculator.CalculatePrice(item.Key, item.Value));
     }
 }
